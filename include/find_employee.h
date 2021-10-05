38ac9df86742051f2e9cf767a3ba27cde1a2d8a3
Naive-C <Naive-C>
2021-10-05 21:53:42 +0900

    Initial commit

---
+++ include/find_employee.h
@@ -0,0 +1,14 @@
+/*
+    EMS - find_employee.h
+    Employee Management System
+	Copyright (C) 2021 Naive-C <naive.c.cell@gmail.com>
+*/
+
+#ifndef FIND_EMPLOYEE_H_
+#define FIND_EMPLOYEE_H_
+
+#include "return_file_length.h"
+
+size_t find_employee(const char* id ,FILE* employee_fp); 
+
+#endif
